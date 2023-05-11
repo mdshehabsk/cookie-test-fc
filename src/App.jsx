358f8d8edc69ff97d5c9aadcd7d06f0e83fd0997
mdshehabsk <mdshehabsk@gmail.com>
2023-05-11 19:22:37 +0600

    response not json

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,8 @@ const apiCallWithFetch = async () => {
     },
     credentials:'include'
   })
-  const data = await res.json()
-  console.log(data)
+  // const data = await res.json()
+  console.log(res)
 }
 function App() {
   async function btnClick() {
