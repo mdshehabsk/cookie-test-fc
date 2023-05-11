ab4bd6a7a64e8e32f7adb22e60afcbf36bfec29d
mdshehabsk <mdshehabsk@gmail.com>
2023-05-11 19:16:17 +0600

    good

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import axios from 'axios'
-import './App.css'
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  async function btnClick ()  {
-      const res = await axios.post('https://cookie-bc.onrender.com/user', undefined , {withCredentials:true})
-      console.log(res)
+  async function btnClick() {
+    const res = await axios.post(
+      "https://cookie-test-mu.vercel.app/user", {}, {
+        withCredentials:true,
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    console.log(res);
   }
   return (
     <div className="App">
-      <button onClick={btnClick} >click me to save cookie</button>
+      <button onClick={btnClick}>click me to save cookie</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
